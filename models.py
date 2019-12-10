93d77dff4c81238a2da168cdc0263dd3b992ebc3
Hannes Kr. Hannesson <hanneskr4@gmail.com>
2019-12-10 13:39:25 +0000

    removed boilerplate code from tutorial

--- models.py
+++ models.py
@@ -18,13 +18,4 @@ class Measurement(db.Model):
     value = db.Column(db.Float, unique=False, nullable=False)
     battery = db.Column(db.Float, unique=False, nullable=False)
 
-    def is_authenticated(self):
-        return True
-    def is_active(self):
-        return True
-    def is_anonymous(self):
-        return False
-    def get_id(self):
-        return self.spotify_id
-
 
